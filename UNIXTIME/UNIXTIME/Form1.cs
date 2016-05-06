6bd71dad234bb908c5561772462e1862c18fb745
lycode404 <1910828091@qq.com>
2016-05-06 22:25:26 +0800

    main code file

--- UNIXTIME/UNIXTIME/Form1.cs
+++ UNIXTIME/UNIXTIME/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,7 +12,6 @@ namespace UNIXTIME
 {
     public partial class Form1 : Form
     {
-        //bool feedback_zero = false;
         public Form1()
         {
             InitializeComponent();
